32e5cf109e3b3bca0430f56a919939eb0c57cf22
AlanW <AlanWisny@hotmail.com>
2018-12-02 18:32:37 +0100

    Add project files.

---
+++ StudentTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
